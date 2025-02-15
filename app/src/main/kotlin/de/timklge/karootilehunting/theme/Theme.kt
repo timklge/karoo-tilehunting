5266a3a9fb98d83c50249ba96a6f9f5f6fe7b743
Tim Kluge <timklge@gmail.com>
2025-02-15 11:07:23 +0100

    Change switch color

--- app/src/main/kotlin/de/timklge/karootilehunting/theme/Theme.kt
+++ app/src/main/kotlin/de/timklge/karootilehunting/theme/Theme.kt
@@ -1,13 +1,24 @@
 package de.timklge.karootilehunting.theme
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.glance.LocalContext
+import de.timklge.karootilehunting.R
 
 @Composable
 fun AppTheme(
     content: @Composable () -> Unit,
 ) {
+    val scheme = lightColorScheme(
+        primary = Color(0xFF214559),
+        secondary = Color(0xFF636363),
+        tertiary = Color(0xFFFEF69A),
+    )
+
     MaterialTheme(
         content = content,
+        colorScheme = scheme
     )
 }
