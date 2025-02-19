cabea9be96ae6f38c3ab8f02e21504de77504ab2
Tim Kluge <timklge@gmail.com>
2025-02-19 22:07:15 +0100

    Increase number of drawn activity lines to 100

--- app/src/main/kotlin/de/timklge/karootilehunting/services/ClusterDrawService.kt
+++ app/src/main/kotlin/de/timklge/karootilehunting/services/ClusterDrawService.kt
@@ -132,7 +132,7 @@ class ClusterDrawService(private val karooSystem: KarooSystemServiceProvider,
 
                             segments.forEach { linesInViewSquare[activity.id] = (LineString.fromLngLats(it)) }
 
-                            if (linesInViewSquare.size > 50){ // Render at most 50 activities
+                            if (linesInViewSquare.size > 100){ // Render at most 100 activities
                                 break@activityLoop
                             }
                         }
